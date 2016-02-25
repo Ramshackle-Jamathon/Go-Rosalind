0689440f1cccb9bce2aad1df7c5f2e35b86f4f80
RamshackleJamathon <josephvandrunen@gmail.com>
2016-02-25 17:58:33 -0500

    more solutions

--- problems/lgis.go
+++ problems/lgis.go
@@ -2,8 +2,9 @@ package problems
 
 import (
     "io/ioutil"
-    //"strings"
-    //"strconv"
+    "strings"
+    "strconv"
+    "math"
 )
 
 func LGIS() {
@@ -15,10 +16,86 @@ func LGIS() {
 }
 
 func SolutionLGIS(s string) string{
-    //lines := strings.Split(s, "\n")
-    //perm := strings.Split(lines[1], " ")
-    //n, err := strconv.Atoi(lines[0])
-    //check(err)
+    lines := strings.Split(s, "\n")
+    perm := strings.Split(lines[1], " ")
+    input := make([]int, len(perm))
+    var err error
+    for i,e := range perm {
+        input[i], err = strconv.Atoi(e)
+        check(err)
+    }
     result := ""
+    for _, e := range LIS(input){
+        result = result + strconv.Itoa(e) + " "
+    }
+    result = result + "\n"
+    for _, e := range LDS(input){
+        result = result + strconv.Itoa(e) + " "
+    }
     return result
+}
+
+func LIS (s []int) []int{
+    l := 0;
+    P := make([]int, len(s));
+    M := make([]int, len(s) + 1);
+    for i := 0; i < len(s); i++ {
+        lo := 1;
+        hi := l;
+        for lo <= hi {
+            mid := int(math.Ceil(float64((lo+hi)/2)))
+            if(s[M[mid]] < s[i]){
+                lo = mid + 1
+            } else {
+                hi = mid-1
+            }
+        }
+        newL := lo;
+
+        P[i] = M[newL-1];
+        M[newL] = i;
+
+        if newL > l{
+            l = newL
+        }
+    }
+    S := make([]int, l);
+    k := M[l]
+    for i := l-1; i >= 0; i--{
+        S[i] = s[k]
+        k = P[k]
+    } 
+    return S
+}
+func LDS (s []int) []int{
+    l := 0;
+    P := make([]int, len(s));
+    M := make([]int, len(s) + 1);
+    for i := 0; i < len(s); i++ {
+        lo := 1;
+        hi := l;
+        for lo <= hi {
+            mid := int(math.Ceil(float64((lo+hi)/2)))
+            if(s[M[mid]] > s[i]){
+                lo = mid + 1
+            } else {
+                hi = mid-1
+            }
+        }
+        newL := lo;
+
+        P[i] = M[newL-1];
+        M[newL] = i;
+
+        if newL > l{
+            l = newL
+        }
+    }
+    S := make([]int, l);
+    k := M[l]
+    for i := l-1; i >= 0; i--{
+        S[i] = s[k]
+        k = P[k]
+    } 
+    return S
 }
